efa94ec88696d31680d94bf08630322d4ff6d9fa
RichardH95 <122812964+RichardH95@users.noreply.github.com>
2023-05-28 16:12:33 -0400

    Update Images

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-const slider = document.querySelector('#carousel');
+const slider = document.querySelector("#carousel");
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
+slider.addEventListener("mousedown", (e) => {
   isDown = true;
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
 
-slider.addEventListener('mouseleave', () => {
+slider.addEventListener("mouseleave", () => {
   isDown = false;
 });
 
-slider.addEventListener('mouseup', () => {
+slider.addEventListener("mouseup", () => {
   isDown = false;
 });
 
-slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX);
+  const walk = x - startX;
   slider.scrollLeft = scrollLeft - walk;
 });
 
-// Scroll to the last image on page load
-window.addEventListener('load', (e) => {
-  const images = slider.querySelectorAll('img');
-  const lastImage = images[images.length - 1];
-  const scrollPos = lastImage.offsetLeft - (slider.offsetWidth / 2) + (lastImage.offsetWidth / 2);
-  slider.scrollLeft = scrollPos;
+// Adjust images based on scroll position
+slider.addEventListener("scroll", (e) => {
+  const images = slider.querySelectorAll("img");
+  images.forEach((img) => {
+    img.style.transform = "translateX(" + slider.scrollLeft * 0.1 + "px)";
+  });
 });
 
-// Adjust images based on scroll position
-slider.addEventListener('scroll', (e) => {
-  const images = slider.querySelectorAll('img');
-  images.forEach(img => {
-    img.style.transform = 'translateX(' + (slider.scrollLeft * 0.1) + 'px)';
-  });
+// Scroll to the last image on page load
+window.addEventListener("load", (e) => {
+  const images = slider.querySelectorAll("img");
+  const lastImage = images[images.length - 1];
+  const scrollPos =
+    lastImage.offsetLeft - slider.offsetWidth / 2 + lastImage.offsetWidth / 2;
+  slider.scrollLeft = scrollPos;
 });
